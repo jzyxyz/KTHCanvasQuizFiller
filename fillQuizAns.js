4d5fa43abeff3a4092f7a858554e787093361541
Zhongyuan <nanomalloc@gmail.com>
2018-10-02 23:22:22 +0200

    now support radiobox and dragdown

--- fillQuizAns.js
+++ fillQuizAns.js
@@ -1,22 +1,65 @@
 
 //Paste the output from getQuizAns.js here.
-var Ans = ["lxc console rta", "sudo ifconfig eth1 172.16.1.2 netmask 255.255.255.0",
+var lastInput = ["lxc console rta", "sudo ifconfig eth1 172.16.1.2 netmask 255.255.255.0",
     "sudo ip addr add 172.16.1.2/24 dev eth1", "sudo route add default gw 172.16.1.1",
     "sudo ip route add default via 172.16.1.1", "sudo vtysh", "configure terminal", "interface eth1",
     "ip address 172.16.251.1/24", "ip forwarding", "router ospf", "router­-id 172.16.251.1",
     "network 172.16.251.0/24 area 0", "network 172.16.1.0/24 area 0", "route bgp 65000",
     "bgp router-id 192.168.10.2", "neighbor 192.168.10.1 remote-as 65001", "network 172.16.251.0/24"]
 var results = [1, 1, 1, 1, 1, 1, 1, 0, 0, 1]
+var radioIdx = [1,0,0,0,0]
+var dragValues = ["Show operating configuration","Show interface status and configuration",
+"Show IP routing table","Show the link-state database summary",
+"Show OSPF interface information","Show OSPF neighbour information",
+"Show a summary of BGP neighbor status","Show BGP routes"] 
 //*****************************************
-var boxNodes = document.querySelectorAll("input[type=text].question_input")
-var qHeaderNodes = document.querySelectorAll("div.header")
+
+
 //Auto fill ans
-boxNodes.forEach((item, idx) => {
-    item.value = Ans[idx];
-})
+var boxNodes = document.querySelectorAll("input[type=text].question_input")
+var fillText = () => {
+    boxNodes.forEach((item, idx) => {
+        item.value = lastInput[idx];
+    })
+}
+var radioNodes = document.querySelectorAll("input[type=radio].question_input")
+var fillRadio = () => {
+    radioNodes.forEach((item, idx) => {
+        item.checked = radioIdx[idx] 
+    })
+}
+var dragNodes = document.querySelectorAll("select")
+var fillDrag = () => {
+    dragNodes.forEach( (select, idx) => {
+        optArr = Array.apply(null, select.options)
+        optArr.forEach(
+            option => {
+                if(option.innerHTML == dragValues[idx]){
+                    option.selected=true
+                }
+            }
+        )
+    })
+}
 //Highlight false ans
-qHeaderNodes.forEach((item, idx)=> {
-    if(results[idx]===0){
-        item.style.backgroundColor = "salmon"  
-    }
-})
+var qHeaderNodes = document.querySelectorAll("div.header")
+var highlight = () => {
+    qHeaderNodes.forEach((item, idx)=> {
+        if(results[idx]===0){
+            item.style.backgroundColor = "salmon"  
+        }
+    })
+}
+
+
+//main
+highlight();
+if(lastInput.length>0){
+    fillText();
+} 
+if(radioIdx.length>0){
+    fillRadio();
+}
+if(dragValues.length>0){
+    fillDrag();
+}
--- getQuizAns.js
+++ getQuizAns.js
@@ -1,26 +1,66 @@
-var nl2arr = (nl, arr) => {
-    for(let i = 0;  i < nl.length; arr.push(nl[i++]))
-    return arr;
+var getLastInput = () => {
+    //convert node list to array
+    let boxValues = []
+
+    boxNodes.forEach((item) => {
+        boxValues.push(item.value)
+    })
+    console.log(`paste the output to the fillQuizAns.js. \n\n
+    var lastInput = [${boxValues.map((str) => { return (`"${str}"`) })}] \n`)
+}
+
+
+
+//get all the headers
+var getResult = () => {
+    let qHeaderNodes = document.querySelectorAll("div.header > span.name.question_name")
+    let results = new Array(qHeaderNodes.length).fill(0);
+    qHeaderNodes.forEach((item, idx) => {
+        if (item.querySelector("span") == null) {
+            //if ans is wrong, get index
+            results[idx] = 1;
+            // 1 represents correct ans
+        }
+    })
+    console.log(`also this one\n\n var results = [${results}]`)
+}
+
+
+//support for radio nodes
+var getRadioInput = () => {
+    let radioIdx = new Array(radioNodes.length).fill(0);
+    radioNodes.forEach((item, idx) => {
+        if (item.checked) {
+            radioIdx[idx] = 1;
+        }
+    })
+    console.log(`paste this one \n\n var radioIdx = [${radioIdx}]`)
+}
+//support or drag-down
+
+var getDragAns = () => {
+    let dragValues = []
+
+    dragNodes.forEach((item) => {
+        dragValues.push(item.innerHTML)
+    })
+    console.log(`paste the output to the fillQuizAns.js. \n\n
+    var dragAns = [${dragValues.map((str) => { return (`"${str}"`) })}] \n`)
+}
+
+
+
+
+var radioNodes = document.querySelectorAll("input[type=radio].question_input")
+if(radioNodes.length){
+    getRadioInput();
 }
 var boxNodes = document.querySelectorAll("input[type=text].question_input")
-//convert node list to array
-var boxValues = []
-
-boxNodes.forEach( (item) => {
-    boxValues.push(item.value)
-})
-console.log(`paste the output to the fillQuizAns.js. \n\n
-var Ans = [${boxValues.map( (str) => { return (`"${str}"`) } )}] \n`)
-
-
-//get all the headers 
-var qHeaderNodes = document.querySelectorAll("div.header > span.name.question_name")
-var results = new Array(qHeaderNodes.length).fill(0);
-qHeaderNodes.forEach( (item, idx) => {
-    if(item.querySelector("span") == null) {
-        //if ans is wrong, get index
-        results[idx] = 1;
-        // 1 represents correct ans
-    }
-})
-console.log(`also this one\n\n var results = [${results}]`)
+if(boxNodes.length){
+    getLastInput();
+}
+var dragNodes = document.querySelectorAll("div.answer_match_right > select > option")
+if(dragNodes.length){
+    getDragAns();
+}
+getResult();
